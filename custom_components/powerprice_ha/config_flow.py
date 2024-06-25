6666f66bf4b9116fe0e91502bbbc517c042aa0f0
LDprg <lukas_4dr@gmx.at>
2024-06-25 20:34:25 +0200

    use reconfigure instead

--- custom_components/powerprice_ha/config_flow.py
+++ custom_components/powerprice_ha/config_flow.py
@@ -6,9 +6,8 @@ import voluptuous as vol
 
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.input_number import DOMAIN as INPUT_NUMBER_DOMAIN
-from homeassistant.config_entries import ConfigFlow, OptionsFlow, ConfigEntry
+from homeassistant.config_entries import ConfigFlow
 from homeassistant.helpers import selector
-from homeassistant.core import callback
 
 from . import const as pp
 
@@ -53,40 +52,27 @@ class PowerPriceHaFlow(ConfigFlow, domain=pp.DOMAIN):
             ),
         )
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: ConfigEntry,
-    ) -> OptionsFlow:
-        """Create the options flow."""
-        return OptionsFlowHandler(config_entry)
-
-
-class OptionsFlowHandler(OptionsFlow):
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(
+    async def async_step_reconfigure(
         self,
         user_input,
     ):
-        """Manage the options."""
-        self.current_config = dict(self.config_entry.data)
+        """Handle a reconfiguration flow initialized by the user."""
+
+        entry = self.hass.config_entries.async_get_entry(self.context["entry_id"])
 
         if user_input is not None:
-            return self.async_create_entry(
-                title="",
-                data=user_input,
+            return self.async_update_reload_and_abort(
+                entry,
+                reason="reconfigure_successful",
+                data={**entry.data, **user_input},
             )
 
         return self.async_show_form(
-            step_id="init",
             data_schema=vol.Schema(
                 {
                     vol.Required(
                         pp.CONF_ENERGY_ENTITY_ID,
-                        default=self.current_config.get(pp.CONF_ENERGY_ENTITY_ID),
+                        default=entry.data.get(pp.CONF_ENERGY_ENTITY_ID),
                     ): selector.EntitySelector(
                         selector.EntitySelectorConfig(
                             domain=[
@@ -97,7 +83,7 @@ class OptionsFlowHandler(OptionsFlow):
                     ),
                     vol.Required(
                         pp.CONF_PRICE_ENTITY_ID,
-                        default=self.current_config.get(pp.CONF_PRICE_ENTITY_ID),
+                        default=entry.data.get(pp.CONF_PRICE_ENTITY_ID),
                     ): selector.EntitySelector(
                         selector.EntitySelectorConfig(
                             domain=[
